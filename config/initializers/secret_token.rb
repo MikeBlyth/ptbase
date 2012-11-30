2c9a05da23bfee2f6b79e0227e8f11fdd356f964
Mike Blyth <mjblyth@gmail.com>
2012-11-30 20:03:32 +0100

    Initial Rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ptbase::Application.config.secret_token = '53bcb7922d83cd1acd56b1ef13fee8171900b78b4e874b319676f608116f65a78602f60b7d362dfa8cc52d11f1451ec81a76a24c8734496aeb25e019ba74c37e'
